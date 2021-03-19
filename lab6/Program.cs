8331881ef9ccd5f3bbe8c2668e947d0f1fc878d3
Nikita Bubriak <31805409+nikitabubriak@users.noreply.github.com>
2021-03-19 16:22:49 +0200

    setup

---
+++ lab6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+/*
+ * Lab 6
+ * Variant 1
+ * 
+ * Розробити структуру даних для зберігання інформації про книги в бібліотеці. 
+ * Книга характеризується: 
+ *       назвою 
+ *      ,прізвищем автора 
+ *      ,вартістю 
+ *      ,датою видання 
+ *      ,видавництвом 
+ *      ,списком інвентарних номерів (книга в кількох примірниках). 
+ * У одного автора може бути декілька книг.
+ */
+
+// LINQ to XML
+
+namespace lab6
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+
+        }
+
+    }
+
+
+}
